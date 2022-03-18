9757f36b294c43ff3652f1ef41487007be62bf0b
Jakob Petersson <github@jakobpetersson.se>
2022-03-18 23:42:33 +0100

    Parse input arguments

---
+++ oat_setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+#
+# Setup args parser
+#
+
+arg_parser = argparse.ArgumentParser(description='OAT Setup')
+
+arg_parser.add_argument(
+    'latitude',
+    type=str,
+    action='store',
+    help="The latitude (decimal degrees), positive northern hemisphere, negative (-) for southern"
+)
+
+arg_parser.add_argument(
+    'longitude',
+    type=str,
+    action='store',
+    help="The longitude (decimal degrees), positive eastern hemisphere, negative (-) for western"
+)
+
+arg_parser.add_argument(
+    'serial_port',
+    type=str,
+    nargs='?',
+    default='/dev/ttyUSB0',
+    help="The serial port path (default: %(default)s)"
+)
+
+args = arg_parser.parse_args()
+
+#
+# Print args
+#
+
+print("--- OAT Setup ---")
+print(f"Serial port: {args.serial_port}")
+print(f"Latitude: {args.latitude}\u00b0")
+print(f"Longitude: {args.longitude}\u00b0")
