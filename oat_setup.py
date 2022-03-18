64a4df356c3ad56239028e8236c539d1753a7dfb
Jakob Petersson <github@jakobpetersson.se>
2022-03-18 23:48:48 +0100

    Set lat and long argument types to float

--- oat_setup.py
+++ oat_setup.py
@@ -11,14 +11,14 @@ arg_parser = argparse.ArgumentParser(description='OAT Setup')
 
 arg_parser.add_argument(
     'latitude',
-    type=str,
+    type=float,
     action='store',
     help="The latitude (decimal degrees), positive northern hemisphere, negative (-) for southern"
 )
 
 arg_parser.add_argument(
     'longitude',
-    type=str,
+    type=float,
     action='store',
     help="The longitude (decimal degrees), positive eastern hemisphere, negative (-) for western"
 )
